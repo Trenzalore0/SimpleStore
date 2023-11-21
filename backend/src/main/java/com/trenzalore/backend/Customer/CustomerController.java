86c437f37593af3c0aa3685882a019ae56d334d5
Trenzalore <kleydson122@gmail.com>
2023-11-21 10:21:50 -0300

    :sparkles: separeted logicals in packges

--- backend/src/main/java/com/trenzalore/backend/Customer/CustomerController.java
+++ backend/src/main/java/com/trenzalore/backend/Customer/CustomerController.java
@@ -1,8 +1,14 @@
 package com.trenzalore.backend.Customer;
 
 import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,32 +16,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.trenzalore.backend.ControllerAbstract;
-
 @RestController
 @RequestMapping("/customer")
-public class CustomerController extends ControllerAbstract<CustomerModel, CustomerRepository> {
+public class CustomerController {
 
-  @Override
+  @Autowired
+  private CustomerRepository customerRepository;
+
+  @GetMapping
+  public ResponseEntity<List<CustomerModel>> index() {
+    return ResponseEntity.ok(customerRepository.findAll());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<CustomerModel> getById(@PathVariable String id) {
+    Optional<CustomerModel> customer = customerRepository.findById(id);
+    if (customer.isPresent()) {
+      return ResponseEntity.ok(customer.get());
+    }
+    return ResponseEntity.notFound().build();
+  }
+
   @PostMapping
   public ResponseEntity<CustomerModel> create(@RequestBody CustomerModel newCustomer, UriComponentsBuilder ucb) {
-    if (repository.existsCustomerByEmail(newCustomer.getEmail())) {
+    if (customerRepository.existsCustomerByEmail(newCustomer.getEmail())) {
       return ResponseEntity.ok(newCustomer);
     }
-    CustomerModel customer = repository.save(newCustomer);
+    CustomerModel customer = customerRepository.save(newCustomer);
     URI customerLocation = ucb.path("/customer/{id}").buildAndExpand(customer.getId()).toUri();
     return ResponseEntity.created(customerLocation).build();
   }
 
-  @Override
   @PutMapping
   public ResponseEntity<CustomerModel> update(@RequestBody CustomerModel obj, UriComponentsBuilder ucb) {
-    if (!repository.existsCustomerByEmail(obj.getEmail())) {
+    if (!customerRepository.existsCustomerByEmail(obj.getEmail())) {
       return ResponseEntity.notFound().build();
     }
-    CustomerModel customer = repository.save(obj);
+    CustomerModel customer = customerRepository.save(obj);
     URI customerLocation = ucb.path("/customer/{id}").buildAndExpand(customer.getId()).toUri();
     return ResponseEntity.created(customerLocation).build();
   }
 
+  @DeleteMapping("/{id}")
+  public void delete(@PathVariable String id) {
+    Optional<CustomerModel> customer = customerRepository.findById(id);
+    if (customer.isPresent()) {
+      customerRepository.delete(customer.get());
+    }
+  }
 }
